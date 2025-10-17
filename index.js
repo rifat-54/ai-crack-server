0dd82d6a6b824009bb0234d29cae7fd6f7753707
rifat-54 <rifat999por@gmail.com>
2025-10-17 16:37:22 +0600

    impliment ai

---
+++ index.js
@@ -0,0 +1,34 @@
+require ('dotenv').config()
+const express=require('express')
+const cors=require('cors')
+const app=express()
+const port=process.env.PORT || 5000
+const { GoogleGenAI }= require("@google/genai");
+const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
+// const model = ai.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+app.get('/test-ai',async(req,res)=>{
+//  const prompt="explain how ai work"
+//  const result=await model.generateContent(prompt)
+//  console.log(result);
+
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "what is go language",
+  });
+  console.log(response.text);
+})
+
+
+
+// defult route
+
+app.get('/',async(req,res)=>{
+    res.send("server is running")
+})
+
+// start server 
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
